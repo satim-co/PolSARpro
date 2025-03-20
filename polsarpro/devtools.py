48be3fcec2e3cde95db34aaf81406aae49d83550
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-03-20 17:24:37 +0100

    helper module to pass parameters to command line

---
+++ polsarpro/devtools.py
@@ -0,0 +1,16 @@
+from collections import OrderedDict
+import re
+
+def parse_psp_parameter_string(input_string):
+    """Convenience function to make cli parameters from strings copied from parameter files."""
+    ordered_dict = OrderedDict()
+    
+    for line in input_string.strip().split("\n"):
+        key, value = re.split(r':\s*', line, maxsplit=1)
+        if value.isdigit():
+            value = int(value)
+        elif value.replace('.', '', 1).isdigit():  # Check for float
+            value = float(value)
+        ordered_dict[key] = value
+    str_param = " ".join([f"-{it[0]} {it[1]}" for it in ordered_dict.items()])
+    return str_param

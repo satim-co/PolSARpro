536476148b15eb83335638de87a72ea16756646a
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-03-26 10:30:14 +0000

    boxcar test

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,7 @@
 import numpy as np
 from polsarpro.util import vec_to_mat, S_to_C3, S_to_C3_dask
 from polsarpro.util import T3_to_C3, T3_to_C3_dask
+from polsarpro.util import boxcar, boxcar_dask
 
 
 def test_vec_to_mat():
@@ -45,3 +46,21 @@ def test_T3_to_C3():
             C3.diagonal(axis1=2, axis2=3).sum().real,
             T3.diagonal(axis1=2, axis2=3).sum().real,
         )
+
+
+def test_boxcar():
+
+    N = 128
+    D = 3
+    v = np.random.rand(N, N, D) + 1j * np.random.rand(N, N, D)
+
+    M = vec_to_mat(v)
+    for fun in [boxcar, boxcar_dask]:
+        param = {"img": M, "dim_az": 5, "dim_rg": 3}
+        M_box = fun(**param)
+        assert M_box.shape == M.shape
+        assert M_box.dtype == M.dtype
+        # output has to be Hermitian
+        assert np.allclose(M_box.transpose((0, 1, 3, 2)), M_box.conj())
+        assert np.allclose(M_box.diagonal(axis1=2, axis2=3).imag, 0)
+

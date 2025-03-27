865b4d14560753fd8295fadc5cf7f4f8711b0e2d
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-03-27 17:35:28 +0000

    fix chunk size for S input and output component type

--- polsarpro/decompositions.py
+++ polsarpro/decompositions.py
@@ -1,9 +1,8 @@
 import numpy as np
 import warnings
-from polsarpro.util import T3_to_C3, T3_to_C3_dask
-from polsarpro.util import S_to_C3, S_to_C3_dask
-from polsarpro.util import _boxcar_core, _T3_to_C3_core
-from polsarpro.util import span, boxcar, boxcar_dask
+from polsarpro.util import boxcar, T3_to_C3, S_to_C3
+from polsarpro.util import _boxcar_core, _T3_to_C3_core, _S_to_C3_core
+from polsarpro.util import span
 from bench import timeit
 import dask.array as da
 
@@ -50,46 +49,8 @@ def freeman(
     return _compute_freeman_components(in_)
 
 
-# @timeit
+@timeit
 def freeman_dask(
-    input_data: np.ndarray,
-    input_poltype: str = "C3",
-    boxcar_size: list[int, int] = [3, 3],
-) -> list[np.ndarray, np.ndarray, np.ndarray]:
-    """Applies the Freeman-Durden decomposition. This decomposition is based on physical modeling
-      of the covariance matrix and returns 3 components Ps, Pd and Pv which are the powers of resp.
-      surface, double bounce and volume backscattering.
-
-    Args:
-        input_data (np.ndarray): Input array, may be a covariance, coherency or Sinclair matrix.
-        input_poltype (str, optional): Polarimetric input type (covariance "C3", coherency "T3" or Sinclair "S"). Defaults to "C3".
-        multilook_size (list[int, int], optional): Multilook dimensions along azimuth and range. Defaults to [1, 1].
-        boxcar_size (list[int, int], optional):  Boxcar dimensions along azimuth and range. Defaults to [3, 3].
-
-    Returns:
-        list[np.ndarray, np.ndarray, np.ndarray]: Ps, Pd and Pv components.
-    """
-    if input_poltype == "C3":
-        in_ = input_data
-    elif input_poltype == "T3":
-        in_ = T3_to_C3_dask(input_data)
-    elif input_poltype == "S":
-        in_ = S_to_C3_dask(input_data)
-    else:
-        raise ValueError("Invalid polarimetric type")
-
-    # pre-processing step, it is recommended to filter the matrices to mitigate speckle effects
-    in_ = boxcar_dask(in_, boxcar_size[0], boxcar_size[1])
-
-    da_in = da.from_array(in_, chunks=(500, 500, 3, 3))
-    out = _compute_freeman_components(da_in)
-    # out = _compute_freeman_components_dask(da_in)
-    return da.compute(out)[0]
-
-
-# prototype using only dask arrays
-# TODO: cover all input cases
-def freeman_full_dask(
     input_data: np.ndarray,
     input_poltype: str = "C3",
     boxcar_size: list[int, int] = [3, 3],
@@ -109,6 +70,7 @@ def freeman_full_dask(
     """
 
     in_ = da.from_array(input_data, chunks="auto")
+    print(in_)
     if input_poltype == "T3":
         in_ = da.map_blocks(
             _T3_to_C3_core,
@@ -116,7 +78,12 @@ def freeman_full_dask(
             dtype="complex64",
         )
     elif input_poltype == "S":
-        in_ = S_to_C3_dask(input_data)
+        in_ = da.map_blocks(
+            _S_to_C3_core,
+            in_,
+            dtype="complex64",
+            chunks=in_.chunksize[:2] + (3, 3),
+        )
     else:
         raise ValueError("Invalid polarimetric type")
 
@@ -208,84 +175,6 @@ def _compute_freeman_components(
     return Ps, Pd, Pv
 
 
-def _compute_freeman_components_dask_old(
-    C3: da.Array,
-) -> tuple[da.Array, da.Array, da.Array]:
-    """Compute Freeman decomposition components.
-
-    Args:
-        C3 (da.Array): A NumPy 3x3 covariance matrix.
-
-    Returns:
-        tuple[da.Array, da.Array, da.Array]: Freeman decomposition components (Ps, Pd, Pv).
-    """
-
-    eps = 1e-30
-
-    # Extract real and imaginary parts
-    c11 = C3[..., 0, 0].real.copy()
-    c13r = C3[..., 0, 2].real.copy()
-    c13i = C3[..., 0, 2].imag.copy()
-    c22 = C3[..., 1, 1].real.copy()
-    c33 = C3[..., 2, 2].real.copy()
-
-    fv = 1.5 * c22
-    c11 -= fv
-    c33 -= fv
-    c13r -= fv / 3
-
-    # Intermediate parameters
-    fd = da.zeros(c11.shape[:2], dtype="float32")
-    fs = da.zeros(c11.shape[:2], dtype="float32")
-    alpha = da.zeros(c11.shape[:2], dtype="float32")
-    beta = da.zeros(c11.shape[:2], dtype="float32")
-
-    # Volume scattering condition
-    cnd1 = (c11 <= eps) | (c33 <= eps)
-    fv = da.where(cnd1, 3 * (c11 + c22 + c33 + 2 * fv) / 8, fv)
-
-    # Compute c13 power
-    pow_c13 = c13r**2 + c13i**2
-
-    # Data conditioning for non-realizable term
-    cnd2 = ~cnd1 & (pow_c13 > c11 * c33)
-    scale_factor = da.where(cnd2, da.sqrt(c11 * c33 / (pow_c13 + eps)), 1)
-    c13r *= scale_factor
-    c13i *= scale_factor
-
-    # Recompute after conditioning
-    pow_c13 = c13r**2 + c13i**2
-
-    # Odd bounce dominates
-    cnd3 = ~cnd1 & (c13r >= 0)
-    alpha = da.where(cnd3, -1, alpha)
-    fd = da.where(cnd3, (c11 * c33 - pow_c13) / (c11 + c33 + 2 * c13r), fd)
-    fs = da.where(cnd3, c33 - fd, fs)
-    beta = da.where(cnd3, da.sqrt((fd + c13r) ** 2 + c13i**2) / (fs + eps), beta)
-
-    # Even bounce dominates
-    cnd4 = ~cnd1 & (c13r < 0)
-    beta = da.where(cnd4, 1, beta)
-    fs = da.where(cnd4, (c11 * c33 - pow_c13) / (c11 + c33 - 2 * c13r), fs)
-    fd = da.where(cnd4, c33 - fs, fd)
-    alpha = da.where(cnd4, da.sqrt((fs - c13r) ** 2 + c13i**2) / (fd + eps), alpha)
-
-    # Compute Freeman components
-    Ps = fs * (1 + beta**2)
-    Pd = fd * (1 + alpha**2)
-    Pv = 8 * fv / 3
-
-    sp = span(C3)
-    min_span, max_span = da.nanmin(sp), da.nanmax(sp)
-    min_span = max(min_span, eps)
-
-    Ps = da.where(Ps < min_span, min_span, da.where(Ps > max_span, max_span, Ps))
-    Pd = da.where(Pd < min_span, min_span, da.where(Pd > max_span, max_span, Pd))
-    Pv = da.where(Pv < min_span, min_span, da.where(Pv > max_span, max_span, Pv))
-
-    return Ps, Pd, Pv
-
-
 def _compute_freeman_components_dask(
     C3: da.Array,
 ) -> tuple[da.Array, da.Array, da.Array]:
@@ -331,7 +220,7 @@ def _compute_freeman_components_dask(
 
     # Odd bounce dominates
     cnd3 = ~cnd1 & (c13r >= 0)
-    alpha = da.where(cnd3, -1, 0)
+    alpha = da.where(cnd3, da.float32(-1), da.float32(0))
     fd = da.where(cnd3, (c11 * c33 - pow_c13) / (c11 + c33 + 2 * c13r), 0)
     fs = da.where(cnd3, c33 - fd, 0)
     arg_sqrt = da.maximum((fd + c13r) ** 2 + c13i**2, eps)
@@ -342,7 +231,7 @@ def _compute_freeman_components_dask(
     beta = da.where(cnd4, 1, beta)
     fs = da.where(cnd4, (c11 * c33 - pow_c13) / (c11 + c33 - 2 * c13r), fs)
     fd = da.where(cnd4, c33 - fs, fd)
-    arg_sqrt = np.maximum((fs - c13r) ** 2 + c13i**2, eps) 
+    arg_sqrt = np.maximum((fs - c13r) ** 2 + c13i**2, eps)
     alpha = da.where(cnd4, da.sqrt(arg_sqrt) / (fd + eps), alpha)
 
     # Compute Freeman components

e08a1d06135a6801a63b43baf3c475cb8cff0db6
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-04-09 15:52:49 +0000

    unit test for h a alpha decomposition

--- tests/test_decompositions.py
+++ tests/test_decompositions.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from polsarpro.util import vec_to_mat
 from polsarpro.decompositions import freeman, freeman_dask
+from polsarpro.decompositions import h_a_alpha 
 
 
 @pytest.mark.filterwarnings("ignore:invalid")
@@ -20,3 +21,21 @@ def test_freeman():
 
             assert all((it.shape == input_data.shape[:2] for it in [Ps, Pd, Pv]))
             assert all((it.dtype == "float32" for it in [Ps, Pd, Pv]))
+
+# @pytest.mark.filterwarnings("ignore:invalid")
+def test_h_a_alpha():
+    N = 128
+    D = 3
+    v = np.random.rand(N, N, D) + 1j * np.random.rand(N, N, D)
+    S = np.random.rand(N, N, 2, 2) + 1j * np.random.rand(N, N, 2, 2).astype("complex64")
+    # fake T3 matrix
+    T3 = vec_to_mat(v).astype("complex64")
+    # TODO: add dask version when ready
+    for fun in [h_a_alpha]:
+        for poltype, input_data in zip(["T3", "S"], [T3, S]):
+            h, a, alpha = fun(
+                input_data=input_data, input_poltype=poltype, boxcar_size=[5, 5]
+            )
+
+            assert all((it.shape == input_data.shape[:2] for it in [h, a, alpha]))
+            assert all((it.dtype == "float32" for it in [h, a, alpha]))

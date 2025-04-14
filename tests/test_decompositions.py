bdfb68a8fc8fcc6eb2b7cbdb920d69f99ea420a5
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-04-14 10:53:52 +0000

    test C3 case as well

--- tests/test_decompositions.py
+++ tests/test_decompositions.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from polsarpro.util import vec_to_mat, T3_to_C3
+from polsarpro.util import vec_to_mat, T3_to_C3, C3_to_T3
 from polsarpro.decompositions import freeman, freeman_dask
 from polsarpro.decompositions import h_a_alpha 
 
@@ -29,11 +29,12 @@ def test_h_a_alpha():
     D = 3
     v = np.random.randn(N, N, D) + 1j * np.random.randn(N, N, D)
     S = np.random.randn(N, N, 2, 2) + 1j * np.random.randn(N, N, 2, 2).astype("complex64")
-    # fake T3 matrix
-    T3 = vec_to_mat(v).astype("complex64")
+    # fake input matrices
+    C3 = vec_to_mat(v).astype("complex64")
+    T3 = C3_to_T3(C3)
     # TODO: add dask version when ready
     for fun in [h_a_alpha]:
-        for poltype, input_data in zip(["T3", "S"], [T3, S]):
+        for poltype, input_data in zip(["C3", "T3", "S"], [C3, T3, S]):
             outputs = fun(
                 input_data=input_data, input_poltype=poltype, boxcar_size=[5, 5]
             )

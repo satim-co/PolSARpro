81f306966d77edcb599dc00125edbcbff1c01e53
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-05-15 14:36:13 +0000

    add license headers

--- polsarpro/decompositions.py
+++ polsarpro/decompositions.py
@@ -1,3 +1,32 @@
+"""
+This code is part of the Python PolSARpro software:
+
+"A re-implementation of selected PolSARPro functions in Python,
+following the scientific recommendations of PolInSAR 2021"
+
+and developed within an ESA funded project with SATIM.
+
+Author: Olivier D'Hondt, 2025.
+Scientific advisors: Armando Marino and Eric Pottier.
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+"""
+
 import numpy as np
 import warnings
 from polsarpro.util import boxcar, T3_to_C3, C3_to_T3, S_to_T3, S_to_C3

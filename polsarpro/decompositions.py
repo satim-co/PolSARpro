ca0d0bf41b4e92b52c6b9d10d9a427300e73d02e
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-07-31 10:16:45 +0000

    adapt decomposition for 4x4 matrices

--- polsarpro/decompositions.py
+++ polsarpro/decompositions.py
@@ -25,7 +25,7 @@ limitations under the License.
 import numpy as np
 import xarray as xr
 import dask.array as da
-from polsarpro.util import boxcar, C3_to_T3, S_to_T3
+from polsarpro.util import boxcar, C3_to_T3, S_to_T3, C4_to_T4
 
 def h_a_alpha(
     input_data: xr.Dataset,
@@ -45,10 +45,10 @@ def h_a_alpha(
             a 3x3 matrix (or 2x2 in the case of Sinclair) per pixel, typically with shape
             (..., 3, 3) or (..., 2, 2) depending on `input_poltype`.
         input_poltype (str, optional): The polarimetric basis of the input data. Supported types are:
+            - "S": Sinclair scattering matrix
             - "C3": Lexicographic covariance matrix
             - "T3": Pauli coherency matrix
-            - "S": Sinclair scattering matrix
-            Defaults to "C3".
+            - "C4" and "T4": 4x4 versions of the above
         boxcar_size (list[int, int], optional): Size of the spatial averaging window to be
             applied before decomposition (boxcar filter). Defaults to [3, 3].
         flags (tuple[str], optional): Parameters to compute and return from the decomposition.
@@ -57,6 +57,7 @@ def h_a_alpha(
             - "anisotropy": Scattering anisotropy (A)
             - "alpha": Mean alpha scattering angle (alpha)
             - "beta", "delta", "gamma", "lambda": Other angular or eigenvalue related parameters
+            - "nhu", "epsilon" additional angles defined only for 4x4 matrices. Will be ignored if not processing 4x4 matrices.
             - "alphas", "betas", "deltas", "gammas", "lambdas": Per-eigenvalue versions of the above
             Defaults to ("entropy", "alpha", "anisotropy").
 
@@ -77,13 +78,17 @@ def h_a_alpha(
         "alpha",
         "beta",
         "delta",
+        "epsilon"
         "gamma",
         "lambda",
+        "nhu",
         "alphas",
         "betas",
         "deltas",
+        "epsilons",
         "gammas",
         "lambdas",
+        "nhus"
     )
     for flag in flags:
         if flag not in possible_flags:
@@ -100,6 +105,10 @@ def h_a_alpha(
     if input_data.poltype == "C3":
         in_ = C3_to_T3(input_data)
     elif input_data.poltype == "T3":
+        in_ = input_data
+    elif input_data.poltype == "C4":
+        in_ = C4_to_T4(input_data)
+    elif input_data.poltype == "T4":
         in_ = input_data
     elif input_data.poltype == "S":
         in_ = S_to_T3(input_data)
@@ -122,32 +131,25 @@ def h_a_alpha(
 
     # remove NaNs to avoid errors in eigh
     mask = in_.to_array().isnull().any("variable")
-    in_ = in_.fillna(1e-30)
+    in_ = in_.fillna(eps)
 
-    # form a T3 matrix array that can be used in eigh
-    # concatenate columns
-    T3_l1 = xr.concat((in_.m11+eps, in_.m12, in_.m13), dim="j")
-    T3_l2 = xr.concat((in_.m12.conj(), in_.m22+eps, in_.m23), dim="j")
-    T3_l3 = xr.concat((in_.m13.conj(), in_.m23.conj(), in_.m33+eps), dim="j")
-    # concatenate lines
-    new_dims_array = new_dims + ("i", "j")
-    T3 = (
-        xr.concat((T3_l1, T3_l2, T3_l3), dim="i").transpose(*new_dims_array)
-        .chunk({new_dims[0]: "auto", new_dims[1]: "auto", "i": 3, "j": 3})
-    )
+    M = _reconstruct_matrix_from_ds(in_)
 
     # eigendecomposition
     meta = (
         np.array([], dtype="float32").reshape((0, 0, 0)),
         np.array([], dtype="complex64").reshape((0, 0, 0, 0)),
     )
-    l, v = da.apply_gufunc(np.linalg.eigh, "(i,j)->(i), (i,j)", T3, meta=meta)
+    l, v = da.apply_gufunc(np.linalg.eigh, "(i,j)->(i), (i,j)", M, meta=meta)
 
     l = l[..., ::-1]  # descending order
     v = v[..., ::-1]
 
     # returns a dict
-    out = _compute_h_a_alpha_parameters(l, v, flags)
+    if in_.poltype in ("S", "T3", "C4"):
+        out = _compute_h_a_alpha_parameters_T3(l, v, flags)
+    elif in_.poltype in ("T4", "C4"):
+        out = _compute_h_a_alpha_parameters_T4(l, v, flags)
 
     return xr.Dataset(
         # add dimension names, account for 2D and 3D outputs
@@ -163,7 +165,7 @@ def h_a_alpha(
 
 # below this line, functions are not meant to be called directly
 
-def _compute_h_a_alpha_parameters(l, v, flags):
+def _compute_h_a_alpha_parameters_T3(l, v, flags):
 
     eps = 1e-30
 
@@ -242,3 +244,150 @@ def _compute_h_a_alpha_parameters(l, v, flags):
     if "lambdas" in flags:
         outputs["lambdas"] = l
     return outputs
+
+def _compute_h_a_alpha_parameters_T4(l, v, flags):
+
+    eps = 1e-30
+
+    # Pseudo-probabilities (normalized eigenvalues)
+    p = np.clip(l / (eps + l.sum(axis=2)[..., None]), eps, 1)
+
+    outputs = {}
+    if "entropy" in flags:
+        H = np.sum(-p * np.log(p), axis=2) / np.float32(np.log(4))
+        outputs["entropy"] = H
+
+    if "anisotropy" in flags:
+        A = (l[..., 1] - l[..., 2]) / (l[..., 1] + l[..., 2] + eps)
+        outputs["anisotropy"] = A
+
+    if "alpha" in flags or "alphas" in flags:
+        # Alpha angles for each mechanism
+        alphas = np.arccos(np.abs(v[:, :, 0, :]))
+        # Convert to degrees
+        alphas *= 180 / np.pi
+
+    if "alpha" in flags:
+        # Mean alpha
+        alpha = np.sum(p * alphas, axis=2)
+        outputs["alpha"] = alpha
+
+    # Extra angles: beta, delta and gamma angles
+    if "beta" in flags or "betas" in flags:
+        # betas = np.atan2(np.abs(v[:, :, 2, :]), eps + np.abs(v[:, :, 1, :]))
+        beta_num = np.sqrt(np.real(v[:, :, 2, :] * v[:, :, 2, :].conj() + v[:, :, 3, :] * v[:, :, 3, :].conj()))
+        betas = np.atan2(beta_num, eps + np.abs(v[:, :, 1, :]))
+        betas *= 180 / np.pi
+
+    if "beta" in flags:
+        beta = np.sum(p * betas, axis=2)
+        outputs["beta"] = beta
+
+    if "epsilon" in flags or "epsilons" in flags:
+        epsilons = np.atan2(np.abs(v[:, :, 3, :]), eps + np.abs(v[:, :, 3, :]))
+        epsilons *= 180 / np.pi
+
+    if "epsilon" in flags:
+        epsilon = np.sum(p * epsilons, axis=2)
+        outputs["epsilon"] = epsilon
+
+    if "delta" in flags or "gamma" in flags or "deltas" in flags or "gammas" in flags:
+        phases = np.atan2(v[:, :, 0, :].imag, eps + v[:, :, 0, :].real)
+
+    if "delta" in flags or "deltas" in flags:
+        deltas = np.atan2(v[:, :, 1, :].imag, eps + v[:, :, 1, :].real) - phases
+        deltas = np.atan2(np.sin(deltas), eps + np.cos(deltas))
+        deltas *= 180 / np.pi
+
+    if "delta" in flags:
+        delta = np.sum(p * deltas, axis=2)
+        outputs["delta"] = delta
+
+    if "gamma" in flags or "gammas" in flags:
+        gammas = np.atan2(v[:, :, 2, :].imag, eps + v[:, :, 2, :].real) - phases
+        gammas = np.atan2(np.sin(gammas), eps + np.cos(gammas))
+        gammas *= 180 / np.pi
+
+    if "gamma" in flags:
+        gamma = np.sum(p * gammas, axis=2)
+        outputs["gamma"] = gamma
+
+    if "nhu" in flags or "nhu" in flags:
+        nhus = np.atan2(v[:, :, 3, :].imag, eps + v[:, :, 3, :].real) - phases
+        nhus = np.atan2(np.sin(nhus), eps + np.cos(nhus))
+        nhus *= 180 / np.pi
+
+    if "nhu" in flags:
+        nhu = np.sum(p * nhus, axis=2)
+        outputs["nhu"] = nhu
+
+    # Average target eigenvalue
+    if "lambda" in flags or "lambdas" in flags:
+        # lambda is a python reserved keyword, using lambd instead
+        lambd = np.sum(p * l, axis=2)
+        outputs["lambda"] = lambd
+
+    # extras outputs: non averaged parameters (ex: alpha1, alpha2, alpha3)
+    if "alphas" in flags:
+        outputs["alphas"] = alphas
+
+    if "betas" in flags:
+        outputs["betas"] = betas
+
+    if "epsilons" in flags:
+        outputs["epsilons"] = epsilons
+
+    if "deltas" in flags:
+        outputs["deltas"] = deltas
+
+    if "gammas" in flags:
+        outputs["gammas"] = gammas
+
+    if "nhus" in flags:
+        outputs["nhus"] = nhus
+
+    if "lambdas" in flags:
+        outputs["lambdas"] = l
+    return outputs
+
+# this is a convenience function to give eigh the right data format
+def _reconstruct_matrix_from_ds(ds):
+
+    eps = 1e-30 
+    
+    if {"y", "x"}.issubset(ds.dims):
+        new_dims = ("y", "x")
+    elif {"lat", "lon"}.issubset(ds.dims):
+        new_dims = ("lat", "lon")
+    else:
+        ValueError(
+            "Input data does not have valid dimension names. ('y', 'x') or ('lat', 'lon') allowed."
+        )
+
+    new_dims_array = new_dims + ("i", "j")
+    if ds.poltype == "T3":
+        # build each line of the T3 matrix
+        T3_l1 = xr.concat((ds.m11+eps, ds.m12, ds.m13), dim="j")
+        T3_l2 = xr.concat((ds.m12.conj(), ds.m22+eps, ds.m23), dim="j")
+        T3_l3 = xr.concat((ds.m13.conj(), ds.m23.conj(), ds.m33+eps), dim="j")
+
+        # Concatenate all lines into a 3x3 matrix
+        return (
+            xr.concat((T3_l1, T3_l2, T3_l3), dim="i").transpose(*new_dims_array)
+            .chunk({new_dims[0]: "auto", new_dims[1]: "auto", "i": 3, "j": 3})
+        )
+    elif ds.poltype == "T4":
+        # build each line of the T4 matrix
+        T4_l1 = xr.concat((ds.m11 + eps, ds.m12,       ds.m13,       ds.m14), dim="j")
+        T4_l2 = xr.concat((ds.m12.conj(), ds.m22 + eps, ds.m23,       ds.m24), dim="j")
+        T4_l3 = xr.concat((ds.m13.conj(), ds.m23.conj(), ds.m33 + eps, ds.m34), dim="j")
+        T4_l4 = xr.concat((ds.m14.conj(), ds.m24.conj(), ds.m34.conj(), ds.m44 + eps), dim="j")
+
+        # concatenate all lines into a 4x4 matrix
+        return (
+            xr.concat((T4_l1, T4_l2, T4_l3, T4_l4), dim="i")
+            .transpose(*new_dims_array)
+            .chunk({new_dims[0]: "auto", new_dims[1]: "auto", "i": 4, "j": 4})
+        ) 
+    else:
+        raise NotImplementedError("Implemented only for T3 and T4 poltypes.")

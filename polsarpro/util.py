caf18476b64655ac7bc83009ebc332bc3ed73df8
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-08-01 13:43:13 +0000

    invert channels for the C-py comparison to succeed

--- polsarpro/util.py
+++ polsarpro/util.py
@@ -162,11 +162,11 @@ def S_to_T4(S: xarray.Dataset) -> xarray.Dataset:
     k1 = ((S.hh + S.vv) / c).astype("complex64", copy=False)
     k2 = ((S.hh - S.vv) / c).astype("complex64", copy=False)
     k3 = ((S.hv + S.vh) / c).astype("complex64", copy=False)
-    k4 = (1j * (S.hv - S.vh) / c).astype("complex64", copy=False)
+    # NOTE: it might work only with ALOS-1 (hv-vh is inverted, according to C-PSP)
+    k4 = (1j * (S.vh - S.hv) / c).astype("complex64", copy=False)
+    # alternate version
+    # k4 = (1j * (S.hv - S.vh) / c).astype("complex64", copy=False)
     
-    # k4 = ((S.vh.imag - S.hv.imag) / c).astype("float32", copy=False)
-    # k4 += 1j * ((S.hv.real - S.vh.real) / c).astype("float32", copy=False)
-
     # compute the Hermitian matrix elements
     T4 = {}
 

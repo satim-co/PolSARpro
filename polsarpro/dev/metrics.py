29c07de78ed15b90400212f6eb336c4fe8294640
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-12-11 10:37:23 +0000

    add helper visualiser for bounded outputs

--- polsarpro/dev/metrics.py
+++ polsarpro/dev/metrics.py
@@ -92,6 +92,7 @@ def summarize_metrics(
 
     return pd.concat(dfs)
 
+# helper function to visualize errors
 def visualize_errors(out_py, out_c):
     import matplotlib.pyplot as plt
 
@@ -113,6 +114,31 @@ def visualize_errors(out_py, out_c):
         plt.colorbar(fraction=0.046, pad=0.04, location="bottom")
         plt.subplot(133)
         plt.imshow(out_py[var][::8].clip(0, 2*m), interpolation="none")
+        plt.colorbar(fraction=0.046, pad=0.04, location="bottom")
+        plt.title("python")
+        plt.axis("off")
+    
+# helper function to visualize errors - version for bounded parameters
+def visualize_errors_noscale(out_py, out_c):
+    import matplotlib.pyplot as plt
+
+    for var in out_py.data_vars:
+        if var not in out_c:
+            Warning(f"Skipping variable '{var}'! Not found in C outputs.")
+        plt.figure(figsize=(10, 6))
+        plt.suptitle(var)
+        plt.subplot(131)
+        plt.imshow((out_py[var] - out_c[var])[::8], interpolation="none")
+        plt.title("Difference: python - C")
+        plt.axis("off")
+        plt.colorbar(fraction=0.046, pad=0.04, location="bottom")
+        plt.subplot(132)
+        plt.imshow(out_c[var][::8], interpolation="none")
+        plt.title("C")
+        plt.axis("off")
+        plt.colorbar(fraction=0.046, pad=0.04, location="bottom")
+        plt.subplot(133)
+        plt.imshow(out_py[var][::8], interpolation="none")
         plt.colorbar(fraction=0.046, pad=0.04, location="bottom")
         plt.title("python")
         plt.axis("off")

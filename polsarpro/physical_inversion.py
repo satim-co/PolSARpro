cd49fcff79e6bb012a09d9bb78d4f654b5cf952f
Olivier D'Hondt <dhondt.olivier@gmail.com>
2026-04-29 14:33:38 +0000

    fix division by zero warnings

--- polsarpro/physical_inversion.py
+++ polsarpro/physical_inversion.py
@@ -91,29 +91,40 @@ def _apply_dubois_inversion(theta, f0, hh, vv, hv, calib, thresh1, thresh2):
     lambd = 100 * 0.3 / f0
     eps = np.finfo(np.float32).eps
 
-    msk_valid = (hv / vv < 10 ** (thresh1 / 10.0)) & (hh / vv < 10 ** (thresh2 / 10.0))
+    theta_valid = np.isfinite(theta) & (theta > 0) & (theta < (np.pi / 2.0))
+    hh_pos = hh > 0
+    vv_pos = vv > 0
+    base_valid = theta_valid & hh_pos & vv_pos
+
+    vv_safe = xr.where(vv_pos, vv, 1.0)
+    hh_safe = xr.where(hh_pos, hh, 1.0)
+    theta_safe = xr.where(theta_valid, theta, 1.0)
+
+    msk_valid = base_valid & (hv / vv_safe < 10 ** (thresh1 / 10.0)) & (
+        hh / vv_safe < 10 ** (thresh2 / 10.0)
+    )
 
     ks_inv = np.exp(
-        1.36905 * np.log(hh)
-        - 0.83333 * np.log(vv)
-        + 0.446425 * np.log(np.cos(theta))
-        + 3.34525 * np.log(np.sin(theta))
+        1.36905 * np.log(hh_safe)
+        - 0.83333 * np.log(vv_safe)
+        + 0.446425 * np.log(np.cos(theta_safe))
+        + 3.34525 * np.log(np.sin(theta_safe))
         - 0.375 * np.log(lambd)
         + 1.78989 * np.log(10)
     )
-    msk_ks = (ks_inv >= 0) & (ks_inv <= np.pi)
+    msk_ks = base_valid & (ks_inv >= 0) & (ks_inv <= np.pi)
     ks_dub = xr.where(msk_valid & msk_ks, ks_inv, 0.0)
 
     ks_safe = xr.where(ks_dub > 0, ks_dub, np.nan)
     er_inv = (
-        np.log10(hh)
-        + np.log10(vv)
+        np.log10(hh_safe)
+        + np.log10(vv_safe)
         + 5.12
-        + np.log10(np.cos(theta))
+        + np.log10(np.cos(theta_safe))
         - 2.5 * np.log10(ks_safe + eps)
         - 1.4 * np.log10(lambd)
-    ) / (0.074 * np.tan(theta) + eps)
-    msk_er = (er_inv > 0) & (er_inv <= 100)
+    ) / (0.074 * np.tan(theta_safe) + eps)
+    msk_er = base_valid & (er_inv > 0) & (er_inv <= 100)
     er_dub = xr.where(msk_valid & msk_ks & msk_er, er_inv, 0.0)
 
     er_safe = xr.where(er_dub > 0, er_dub, np.nan)
@@ -123,7 +134,7 @@ def _apply_dubois_inversion(theta, f0, hh, vv, hv, calib, thresh1, thresh2):
         - 5.5e-4 * (er_safe * er_safe)
         + 4.3e-6 * (er_safe * er_safe * er_safe)
     ) * 100
-    msk_mv = (mv_inv > 0) & (mv_inv <= 100)
+    msk_mv = base_valid & (mv_inv > 0) & (mv_inv <= 100)
     mv_dub = xr.where(msk_valid & msk_ks & msk_er & msk_mv, mv_inv, 0.0)
 
     msk_out = (msk_valid & msk_ks & msk_er & msk_mv).astype(np.float32)
